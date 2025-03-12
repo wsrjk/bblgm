fc52f2eaca939b54f16526551ba5e7f2a4b42f8a
wsrjk <jeetendra.r@willowstonerealty.in>
2025-03-12 17:51:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -107,7 +107,10 @@ if __name__ == '__main__':
 
 import os
 
+import os
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
 
