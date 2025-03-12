961c813dc3877986ec69aa0bf30efde31b84bc8e
wsrjk <jeetendra.r@willowstonerealty.in>
2025-03-12 17:36:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -104,3 +104,10 @@ def score_increment(bubble):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
